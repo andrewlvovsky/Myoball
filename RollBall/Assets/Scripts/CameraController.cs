717c1a15e4d00b96847e8fdaf57184c63fa889ae
GrantNasution <36121358+GrantNasution@users.noreply.github.com>
2018-02-04 09:10:16 -0800

    Leap motion controls
    implemented leap motion controls, and added andrews camera script

--- RollBall/Assets/Scripts/CameraController.cs
+++ RollBall/Assets/Scripts/CameraController.cs
@@ -8,13 +8,27 @@ public class CameraController : MonoBehaviour {
 
     private Vector3 offset;
 
+    private float xPos = 0.0f, yPos = 6.0f, zPos = -17.0f;
+    private float levelUp = 9.5f, levelDown = -9.5f, factor = 10f;
+
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
+        Debug.Log("HEY STUPID");
+        transform.position = new Vector3(xPos, yPos, zPos);
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+	void Update () {
+        //Debug.Log(player.transform.position.y);
+
+        if (player.transform.position.y >= levelUp || player.transform.position.y <= levelDown)
+        {
+            yPos -= factor;
+            transform.position = new Vector3(xPos, yPos, zPos);
+
+            levelDown -= 10f;
+            levelUp += 10f;
+        }
 	}
 }
--- RollBall/Assets/Scripts/StageController.cs
+++ RollBall/Assets/Scripts/StageController.cs
@@ -1,45 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Leap;
+using Leap.Unity;
 using UnityEngine;
 
 public class StageController : MonoBehaviour {
 
-    public const float rotationSpeed = 0.1f;
+    //Multiplier making pitch and roll rotation movements more pronounced
+    public const float rotationSpeed = 15;
+    //Leap does not record palm pitch and roll to positive values therefore must be offset
+    public const float pitchOffset = 1.3f;
+    public const float rollOffset = 0.75f;
+    public Controller controller;
+    private float pitch, roll;
+    //Records previous pitch and roll values to discern inaccurate readings
+    private float prevPitch, prevRoll;
     private float x, y, z;
+
     // Use this for initialization
     void Start () {
         x = y = z = 0.0f;
+        pitch = roll = prevPitch = prevRoll = 0.0f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        bool down, up, left, right;
-        down = up = left = right = false;
-        if (Input.GetKey(KeyCode.DownArrow))
+        controller = new Controller();
+        List<Hand> hands = controller.Frame().Hands;
+        if(hands.Count > 0)
         {
-            x -= rotationSpeed;
-            down = true;
+            Hand hand = hands[0];
+            //Pitch multiplied by -1 cause for some reason positive pitch angles in opposite direction
+            pitch = (hand.PalmNormal.Pitch + pitchOffset) * rotationSpeed * -1;
+            roll = (hand.PalmNormal.Roll + rollOffset) * rotationSpeed;
+            if (Mathf.Abs(prevRoll - roll) > 22.0f && prevRoll != 0.0f)
+            {
+                Debug.Log("Roll Spike " + Mathf.Abs(prevRoll - roll));
+                roll = prevRoll;
+            }
+            if(Mathf.Abs(prevPitch - pitch) > 22.0f && prevPitch != 0.0f)
+            {
+                Debug.Log("Pitch Spike " + Mathf.Abs(prevPitch - pitch));
+                pitch = prevPitch;
+            }
+            transform.rotation = Quaternion.Euler(new Vector3(pitch, 0, roll));
+            prevRoll = roll;
+            prevPitch = pitch;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            x += rotationSpeed;
-            up = true;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            z += rotationSpeed;
-            left = true;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            z -= rotationSpeed;
-            right = true;
-        }
-        if (!down && !up)
-            x = 0.0f;
-        if (!left && !right)
-            z = 0.0f;
-        Vector3 rotate = new Vector3(x, y, z);
-        transform.Rotate(rotate);
     }
 }
